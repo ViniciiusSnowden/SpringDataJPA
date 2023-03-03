d74121f9a724fe01ff4493299fbecbc3314d8eaa
Paulo Vinicius Silva Costa <paulo.costa@mv.com.br>
2023-03-03 15:32:26 -0300

    [implementation] Reports find date contration

--- src/main/java/br/com/pauloviniciius/SpringData/repository/FuncionarioRepository.java
+++ src/main/java/br/com/pauloviniciius/SpringData/repository/FuncionarioRepository.java
@@ -2,9 +2,21 @@ package br.com.pauloviniciius.SpringData.repository;
 
 import br.com.pauloviniciius.SpringData.orm.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
     List<Funcionario> findByNome(String nome);
+
+    //JPQL
+    @Query("SELECT f FROM Funcionario  f where f.nome = :nome " +
+            "and f.salario >= :salario and f.dataContratacao = :data")
+    List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, Double salario, LocalDate data);
+
+    //NativeQuery
+    @Query(value = "SELECT * FROM funcionario f where f.data_contratacao >= :data", nativeQuery = true)
+    List<Funcionario> findDataContracacaoMaior(LocalDate data);
+
 }
--- src/main/java/br/com/pauloviniciius/SpringData/service/RelatorioService.java
+++ src/main/java/br/com/pauloviniciius/SpringData/service/RelatorioService.java
@@ -5,14 +5,16 @@ import br.com.pauloviniciius.SpringData.repository.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 @Service
 public class RelatorioService {
-
     @Autowired
     private FuncionarioRepository funcionarioRepository;
 
+    private  final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     private boolean system = true;
     public void inicial(Scanner scanner) {
 
@@ -20,8 +22,8 @@ public class RelatorioService {
             System.out.println("Qual ação você quer executar");
             System.out.println("0 - Sair");
             System.out.println("1 - Buscar Funcionario por nome");
-//        System.out.println("2 - Atualizar ");
-//        System.out.println("3 - Vizualizar ");
+            System.out.println("2 - Buscar maior salario por nome");
+            System.out.println("3 - Busca funcionario data contratacao ");
 //        System.out.println("4 - Deletar ");
 
             int action = scanner.nextInt();
@@ -30,12 +32,12 @@ public class RelatorioService {
                 case 1:
                     buscaFuncionarioNome(scanner);
                     break;
-//            case 2:
-//                atualizar(scanner);
-//                break;
-//            case 3:
-//                listar();
-//                break;
+                case 2:
+                    buscaFuncioaniroNomeSalarioMaiorData(scanner);
+                    break;
+                case 3:
+                    buscaFuncionarioDataContratcao(scanner);
+                    break;
 //            case 4:
 //                deletar(scanner);
                 default:
@@ -49,6 +51,30 @@ public class RelatorioService {
         System.out.println("Qual nome deseja pesquisar:");
         List<Funcionario> funcianrios = funcionarioRepository.findByNome(scanner.next());
         funcianrios.forEach(System.out::println);
+    }
+
+    private void buscaFuncioaniroNomeSalarioMaiorData(Scanner scanner){
+        System.out.printf("Qual nome funcionario deseja buscar");
+        String nome = scanner.next();
+
+        System.out.println("Qual data de contratacao");
+        String data = scanner.next();
+        LocalDate localDate = LocalDate.parse(data, formatter);
+
+        System.out.println("Qual salario");
+        Double salario  = scanner.nextDouble();
+
+        List<Funcionario> list = funcionarioRepository.findNomeSalarioMaiorDataContratacao(nome,salario, localDate);
+        list.forEach(System.out::println);
+
+    }
+
+    private void buscaFuncionarioDataContratcao(Scanner scanner){
+        System.out.println("Qual data contratacao");
+        String data = scanner.next();
+        LocalDate localDate = LocalDate.parse(data, formatter);
+        List<Funcionario> list = funcionarioRepository.findDataContracacaoMaior(localDate);
+        list.forEach(System.out::println);
     }
 
 }
