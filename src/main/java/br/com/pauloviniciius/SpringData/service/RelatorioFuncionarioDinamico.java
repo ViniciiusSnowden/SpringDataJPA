0ac4a90f76971a6686011206478d8fecba1c6dc9
Paulo Vinicius Silva Costa <paulo.costa@mv.com.br>
2023-03-23 19:51:31 -0300

    Specification

--- src/main/java/br/com/pauloviniciius/SpringData/service/RelatorioFuncionarioDinamico.java
+++ src/main/java/br/com/pauloviniciius/SpringData/service/RelatorioFuncionarioDinamico.java
@@ -5,10 +5,12 @@ import br.com.pauloviniciius.SpringData.repository.FuncionarioRepository;
 import br.com.pauloviniciius.SpringData.specification.SpecificantionFuncionario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Scanner;
 
+@Service
 public class RelatorioFuncionarioDinamico {
 
     @Autowired
@@ -18,6 +20,33 @@ public class RelatorioFuncionarioDinamico {
     public void inicial(Scanner scanner){
         System.out.printf("Digite um nome");
         String nome = scanner.next();
+
+        if(nome.equalsIgnoreCase("NULL")){
+            nome = null;
+        }
+
+        System.out.printf("Digite um nome");
+        String cpf = scanner.next();
+
+        if(cpf.equalsIgnoreCase("NULL")){
+            cpf = null;
+        }
+
+        System.out.printf("Digite um nome");
+        Double salario = scanner.nextDouble();
+
+        if(salario == null){
+            salario = null;
+        }
+
+
+        System.out.printf("Digite um Data Contracacao");
+        String dataContratacao = scanner.next();
+
+        if(nome.equalsIgnoreCase("NULL")){
+            nome = null;
+        }
+
 
         List<Funcionario> funcionarios = funcionarioRepository.findAll(Specification.where(SpecificantionFuncionario.nome(nome)));
     }
--- src/main/java/br/com/pauloviniciius/SpringData/specification/SpecificantionFuncionario.java
+++ src/main/java/br/com/pauloviniciius/SpringData/specification/SpecificantionFuncionario.java
@@ -3,6 +3,8 @@ package br.com.pauloviniciius.SpringData.specification;
 import br.com.pauloviniciius.SpringData.orm.Funcionario;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.time.LocalDate;
+
 public class SpecificantionFuncionario {
 
     public static Specification<Funcionario> nome(String nome) {
@@ -17,11 +19,11 @@ public class SpecificantionFuncionario {
 
     public static Specification<Funcionario> salario(Double salario) {
         return (root, criteriaQuery, criteriaBuilder) ->
-                criteriaBuilder.like(root.get("salario"),"%" + salario +"%");
+                criteriaBuilder.greaterThan(root.get("salario"), salario );
     };
 
-    public static Specification<Funcionario> nome(String nome) {
+    public static Specification<Funcionario> dataContratacao(LocalDate dataContratacao) {
         return (root, criteriaQuery, criteriaBuilder) ->
-                criteriaBuilder.like(root.get("nome"),"%" + nome +"%");
+                criteriaBuilder.greaterThan(root.get("dataContratacao"), dataContratacao);
     };
 }
